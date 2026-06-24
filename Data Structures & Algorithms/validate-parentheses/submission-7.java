c293a77ea6891f2df5c5af13c93a40d113dd5156
Sathappan <sathappanas1989@gmail.com>
2026-06-24 15:38:49 -0700

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch == '{' || ch == '[' || ch == '(') {
+                stack.push(ch);
+            } else if (ch == '}' && !stack.empty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (ch == ']' && !stack.empty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (ch == ')' && !stack.empty() && stack.peek() == '(') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.empty() ? true : false;
+    }
+}
